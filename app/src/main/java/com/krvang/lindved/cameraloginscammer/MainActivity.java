868a7d8c8e71f92cd3911c14d7e5c29dce8c83b2
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-03-08 10:28:16 +0100

    Now checking for the right combination

--- app/src/main/java/com/krvang/lindved/cameraloginscammer/MainActivity.java
+++ app/src/main/java/com/krvang/lindved/cameraloginscammer/MainActivity.java
@@ -10,18 +10,32 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "Test";
+    private final int LENGTH_OF_CREDENTIAL = 4;
+    private final int MAX_ATTEMPTS = 3;
+    private final List<Integer> CREDENTIALS = new ArrayList<>(Arrays.asList(2, 6, 8, 4));
 
     private LinearLayout mLinearLayout;
     private TextView mMessageTextView;
+
+    private List<Integer> mNumbers;
+    private int mTries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNumbers = new ArrayList<>();
+        mTries = 0;
         mLinearLayout = findViewById(R.id.llDigits);
         createDigits();
     }
@@ -40,7 +54,31 @@ public class MainActivity extends AppCompatActivity {
             mLinearLayout.addView(row);
         }
     }
-    
+
+    private void addNumber(int number){
+        mNumbers.add(number);
+        if(mNumbers.size() == LENGTH_OF_CREDENTIAL){
+            mTries++;
+            boolean match = checkCredentialMatch();
+
+
+            if(!match && mTries == MAX_ATTEMPTS){
+                //TODO RKL: Take picture here.
+            }
+
+            mTries = mTries == MAX_ATTEMPTS ? 0 : mTries;
+            mNumbers = new ArrayList<>();
+        }
+    }
+
+    private boolean checkCredentialMatch(){
+        for(int i = 0; i < mNumbers.size(); i++){
+            if(!Objects.equals(mNumbers.get(i), CREDENTIALS.get(i)))
+                return false;
+        }
+        return true;
+    }
+
 
     private class Digit extends android.support.v7.widget.AppCompatButton implements View.OnClickListener{
 
@@ -55,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            Log.d(TAG, "onClick: " + mNumber + " button is clicked");
+            addNumber(mNumber);
         }
     }
 }
